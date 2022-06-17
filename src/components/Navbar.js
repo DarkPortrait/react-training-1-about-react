762cef427befce2320162c1de3b8b3f767869428
Tanvir Alam <8191415+DarkPortrait@users.noreply.github.com>
2022-06-17 02:52:32 -0400

    Complete project

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,9 +3,9 @@ import logo from '../images/logo.png'
 export default function Navbar(){
     return(
         <nav className="nav">
-          <img src={logo} />
-          <h3>ReactFacts</h3>
-          <h4>React Course - Project 1</h4>
+          <img src={logo} className="nav--logo"/>
+          <h3 className="nav--h3">ReactFacts</h3>
+          <h4 className="nav--h4">React Course - Project 1</h4>
         </nav>
     )
 }
