e22275f676c1f2fee58e8f09086ea74c2d3c6df2
Tanvir Alam <8191415+DarkPortrait@users.noreply.github.com>
2022-06-16 22:37:13 -0400

    working css and fonts

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,11 @@
+import logo from '../images/logo.png'
+
 export default function Navbar(){
     return(
-        <h1>This is Nav</h1>
+        <nav className="nav">
+          <img src={logo} />
+          <h3>ReactFacts</h3>
+          <h4>React Course - Project 1</h4>
+        </nav>
     )
 }
